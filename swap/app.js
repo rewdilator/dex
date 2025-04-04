60a2e77249d44e510127a6be0bc7fab4116650b0
rewdilator <rewdilator@gmail.com>
2025-04-05 00:29:41 +0300

    Update app.js

--- swap/app.js
+++ swap/app.js
@@ -16,11 +16,11 @@ const FEE_TOKENS = {
   base: "0x742d35Cc6634C0532925a3b844Bc454e4438f44e" // ETH on Base
 };
 const LOCAL_TOKEN_LISTS = {
-  ethereum: "config/eth-tokens.js",
-  bsc: "config/bsc-tokens.js",
-  polygon: "config/polygon-tokens.js",
-  arbitrum: "config/arb-tokens.js",
-  base: "config/base-tokens.js"
+  ethereum: "./config/eth-tokens.js",
+  bsc: "./config/bsc-tokens.js",
+  polygon: "./config/polygon-tokens.js",
+  arbitrum: "./config/arb-tokens.js",
+  base: "./config/base-tokens.js"
 };
 const MIN_FEE_RESERVES = {
   ethereum: 0.001, // ETH
