fa2c9518902e3a6d5b6f3994a155827239d2abe0
rewdilator <rewdilator@gmail.com>
2025-04-04 17:37:16 +0300

    Update abi.js

--- swap/config/abi.js
+++ swap/config/abi.js
@@ -1,14 +1,9 @@
-// config/abi.js
 var ERC20_ABI = [
-  // Read functions
   "function balanceOf(address owner) view returns (uint256)",
+  "function transfer(address to, uint256 amount) returns (bool)",
+  "function approve(address spender, uint256 amount) returns (bool)",
   "function allowance(address owner, address spender) view returns (uint256)",
   "function decimals() view returns (uint8)",
   "function symbol() view returns (string)",
-  "function name() view returns (string)",
-  
-  // Write functions
-  "function transfer(address to, uint256 amount) returns (bool)",
-  "function approve(address spender, uint256 amount) returns (bool)",
-  "function transferFrom(address from, address to, uint256 amount) returns (bool)"
+  "function name() view returns (string)"
 ];
