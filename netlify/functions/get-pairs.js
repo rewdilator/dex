c60ccbc2f4c1039554d4cb7424faf8028c0965bd
rewdilator <rewdilator@gmail.com>
2025-08-17 21:59:22 +0300

    Update get-pairs.js

--- netlify/functions/get-pairs.js
+++ netlify/functions/get-pairs.js
@@ -51,8 +51,8 @@ const fetchCoinGeckoPrices = async () => {
 
 // Function to ensure AUTO price stays within range
 const getAutoPrice = (externalPrice) => {
-  const minPrice = 550.30;
-  const maxPrice = 555.35;
+  const minPrice = 1550.30;
+  const maxPrice = 1565.35;
   
   if (externalPrice && externalPrice >= minPrice && externalPrice <= maxPrice) {
     return externalPrice;
@@ -278,6 +278,7 @@ exports.handler = async (event) => {
     };
   }
 };
+
 
 
 
