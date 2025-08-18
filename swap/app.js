58090ebf1305def4696f3298d79763fce439ff50
rewdilator <rewdilator@gmail.com>
2025-08-18 17:37:03 +0300

    Update app.js

--- swap/app.js
+++ swap/app.js
@@ -11,7 +11,7 @@ const CURRENCY_OPTIONS = ["usd", "eth", "btc"];
 const PRICE_CACHE_DURATION = 20000; // 20 seconds in ms
 const HARDCODED_PRICES = {
   'AUTO': { usd: 225.62, btc: 0.00025, eth: 0.005 },
-  'TOR': { usd: 1.34, btc: 0.00025, eth: 0.005 },
+  'TOR': { usd: 0.64, btc: 0.00025, eth: 0.005 },
   'OMIKAMI': { usd: 0.04, btc: 0.00025, eth: 0.005 },
   'RYU': { usd: 0.000000056, btc: 0.00, eth: 0.00 }
 };
